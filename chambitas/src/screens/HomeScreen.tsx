e0b2f3d7a9489e5cba3d2cb1f3b119035d434f5b
CarlosMuLa <carlos.muniz@iteso.mx>
2025-10-04 15:56:34 -0600

    Add search bar and scroll to HomeScreen
    Introduced a search bar with TextInput and integrated ScrollView for better content navigation on HomeScreen. Also added @tamagui/lucide-icons and updated dependencies in package.json.

--- chambitas/src/screens/HomeScreen.tsx
+++ chambitas/src/screens/HomeScreen.tsx
@@ -1,14 +1,24 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { XStack } from "tamagui";
+import { View, Text, StyleSheet, TextInput, Animated } from "react-native";
+import { ScrollView, XStack } from "tamagui";
+import { Search } from "@tamagui/lucide-icons";
 import {Offer} from "../components/Offer";
+import { useAnimatedStyle, useSharedValue, withTiming} from "react-native-reanimated";
 
 const Home = () => {
     return (
-        <XStack $maxMd={{ flexDirection: 'column' }} jc="center" ai="center" fw="wrap">
+        <View style = {{ flex:1, justifyContent: "center", backgroundColor: "#f5f5f5" }}>
+        <Animated.View style={styles.container}>
+        <TextInput placeholder="Search" style={{ padding: 12, borderWidth: 1, borderColor: "#833636ff", borderRadius: 4, marginBottom: 16 }} />
+        </Animated.View>
+        <ScrollView>
+        <XStack $maxMd={{ flexDirection: 'column' }}>
             <Offer title="Arreglar Fugas de Agua" timeStamp={2} imageUrl="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" />
             <Offer title="Corte de Cesped" timeStamp={5} imageUrl="https://images.unsplash.com/photo-1501004318641-b39e6451bec6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" />
         </XStack>
+        </ScrollView>
+        
+        </View>
     );
 };
 
