9f3944050b78aefb703ae285c267bde54ad3211d
CarlosMuLa <carlos.muniz@iteso.mx>
2025-10-04 14:01:09 -0600

    Add Tamagui and Reanimated Babel plugins
    Updated babel.config.js to include Tamagui and transform-inline-environment-variables plugins for improved environment handling and Tamagui support. Updated dependencies in package.json and package-lock.json to add required Babel plugins and Tamagui packages.

--- chambitas/babel.config.js
+++ chambitas/babel.config.js
@@ -1,10 +1,26 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      // This is the required plugin for Reanimated
+      // NOTE: this is required to pass the right environment
+      [
+        'transform-inline-environment-variables',
+        {
+          include: ['TAMAGUI_TARGET'],
+        },
+      ],
+      // NOTE: this is optional, you don't *need* the compiler
+      [
+        '@tamagui/babel-plugin',
+        {
+          components: ['tamagui'],
+          config: './tamagui.config.ts',
+          logTimings: true,
+        },
+      ],
+      // NOTE: this is only necessary if you use reanimated for animations
       'react-native-worklets/plugin',
     ],
-  };
-};
+  }
+}
