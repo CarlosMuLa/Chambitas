05f3d8fe2ab7095cdd86a55ce7b3e8b7e2f9431b
CarlosMuLa <116656412+CarlosMuLa@users.noreply.github.com>
2025-10-20 16:54:15 -0600

    Add Tamagui config and update LoginScreen
    Added the Tamagui configuration file to support Tamagui components. Updated dependencies in package.json and package-lock.json. Modified LoginScreen.tsx, likely to integrate or prepare for Tamagui usage.

--- chambitas/src/screens/LoginScreen.tsx
+++ chambitas/src/screens/LoginScreen.tsx
@@ -2,22 +2,30 @@ import { X } from "@tamagui/lucide-icons";
 import React from "react";
 import { View, Text, StyleSheet, TextInput} from "react-native";
 import { XStack, YStack , H2, Paragraph, Label, Input, Button} from "tamagui";
+import { LinearGradient } from "@tamagui/linear-gradient";
 
 const LoginScreen = () => {
     return (
         <YStack style={styles.container} gap="$2">
+            <LinearGradient
+                colors= {['#d6ef75ff', '#3b5998']}
+                start={{ x: 0, y: 1 }}
+                end={{ x: 0, y: 0 }}
+                >
             <H2 style={styles.title}>Bienvenido a Grinder</H2>
+            </LinearGradient>
             <Paragraph>Inicia sesión para continuar</Paragraph>
             <YStack>
                 <YStack >
                     <Label htmlFor="emailInput">Correo Electrónico</Label>
-                    <Input id="emailInput" placeholder="Ingresa tu correo electrónico" size = "$4" />
+                    <Input id="emailInput" placeholder="Ingresa tu correo" size = "$4" width="100%" />
                 </YStack>
                 <YStack>
                     <Label htmlFor="passwordInput">Contraseña</Label>
-                    <Input id="passwordInput" placeholder="Ingresa tu contraseña" secureTextEntry size = "$4"/>
+                    <Input id="passwordInput" placeholder="Ingresa tu contraseña" secureTextEntry size = "$4" width="100%"/>
                 </YStack>
                 <Button size="$4" theme="accent" mt="$4">Iniciar Sesión</Button>
+                <Text style={{ marginTop: 20, textAlign: 'center' }}>¿No tienes una cuenta? Regístrate</Text>
             </YStack>
         </YStack>
     );
